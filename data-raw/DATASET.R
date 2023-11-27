422003f2632dd222581399ecdf7b186cf69fdafa
Sarah Susnea <ssusnea@smith.edu>
2023-11-27 16:04:42 -0500

    added a note to the data raw .R file

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,6 @@
 ## code to prepare `DATASET` dataset goes here
 
+# in this R file we should write code that will clean the case study csv file that we include in the install folder.
+# a user who runs this script should be able to get the same cleaned data frame every time.
+
 usethis::use_data(DATASET, overwrite = TRUE)
