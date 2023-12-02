5ee2ee38fd846eb8da0c7963ec25e6fcb4a44581
c-kelly28 <ckelly@smith.edu>
2023-12-02 11:44:38 -0500

    changed names of datasets (for easy identification)

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,4 +3,6 @@
 # in this R file we should write code that will clean the case study csv file that we include in the install folder.
 # a user who runs this script should be able to get the same cleaned data frame every time.
 
+
+
 usethis::use_data(DATASET, overwrite = TRUE)
